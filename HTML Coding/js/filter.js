6913172feef3009148d86d7f6091a292990542d1
Kirill <kirillaromad@mail.ru>
2020-11-11 17:07:03 +0300

    fix like button and js

--- HTML Coding/js/filter.js
+++ HTML Coding/js/filter.js
@@ -1,11 +1,13 @@
-let coll = document.getElementsByClassName('collapsible');
-for (let i=0; i<coll.length; i++) {
-    coll[i].addEventListener('click', function() {
-        let content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + 'px'
-        }
-    })
-}
+window.onload = function () {
+    let coll = document.getElementsByClassName('filter-button');
+    for (let i=0; i<coll.length; i++) {
+        coll[i].addEventListener('click', function() {
+            let content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + 'px'
+            }
+        })
+    }
+};
