8a29863b122f7645254429b9c8cd27f0bb45b001
Kirill <kirillaromad@mail.ru>
2020-11-11 17:57:36 +0300

    add search for keywords in filter

--- HTML Coding/js/filter-hidden.js
+++ HTML Coding/js/filter-hidden.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+
     let coll = document.getElementsByClassName('filter-button');
     for (let i=0; i<coll.length; i++) {
         coll[i].addEventListener('click', function() {
@@ -8,6 +9,21 @@ window.onload = function () {
             } else {
                 content.style.maxHeight = content.scrollHeight + 'px'
             }
-        })
+        });
     }
-};
+
+    let search = document.getElementById('search-keyword');
+    let keywords = document.getElementsByClassName('filter-keyword'); 
+    search.addEventListener('keyup', function () {
+        let phrase = search.value.toUpperCase();
+        for (i=0; i<keywords.length; i++) {
+            let key = keywords[i].getElementsByTagName('label')[0];
+            if (key.innerHTML.toUpperCase().indexOf(phrase) > -1) {
+                keywords[i].style.display = "";
+            } else {
+                keywords[i].style.display = "none";
+            }
+        }
+    });
+
+}
