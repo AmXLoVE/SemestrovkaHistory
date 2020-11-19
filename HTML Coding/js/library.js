38cb37436c1407a15f0413661409829a66256b33
Kirill <kirillaromad@mail.ru>
2020-11-20 01:57:47 +0300

    fix searching in table

--- HTML Coding/js/library.js
+++ HTML Coding/js/library.js
@@ -1,6 +1,7 @@
 window.onload = function () {
 
     const allInputs = document.querySelectorAll('input[type="text"]');
+
     for (let inp of allInputs){
         inp.value = "";
     }
@@ -9,8 +10,14 @@ window.onload = function () {
 
     const table = document.querySelector('table');
     let search = document.getElementById('search-text');
+    let select = document.querySelector('select');
 
-    search.addEventListener('keyup', function () {
+    function searchFunc () {
+
+        let notation = document.getElementsByClassName('library-notation')[0];
+        if (typeof notation !== "undefined") {
+            notation.remove();
+        }
         
         let filter = document.getElementsByClassName('filter')[0].value.toUpperCase();
         let phrase = search.value.toUpperCase();
@@ -22,16 +29,38 @@ window.onload = function () {
                 break;
             }
         }
+
+        let noneCount = 0;
     
         for (var i = 1; i < table.rows.length; i++) {
             if (table.rows[i].cells[cellIndex].innerHTML.toUpperCase().indexOf(phrase) > -1) {
                 table.rows[i].style.display = "";
             } else {
                 table.rows[i].style.display = "none";
+                noneCount++;
             }
         }
-    
-    });
+
+        if (filter === 'DISABLED' && noneCount === table.rows.length - 1) {
+            let div = document.createElement('div');
+            div.innerHTML = 'Выберите фильтр';
+            div.classList.add('library-notation');
+            let main = document.getElementsByClassName('main-block-library')[0];
+            main.appendChild(div);
+            return;
+        }
+
+        if (noneCount === table.rows.length - 1) {
+            let div = document.createElement('div');
+            div.innerHTML = 'Ничего не найдено :(';
+            div.classList.add('library-notation');
+            let main = document.getElementsByClassName('main-block-library')[0];
+            main.appendChild(div);
+        }
+    }
+
+    search.addEventListener('keyup', searchFunc);
+    select.addEventListener('change', searchFunc);
 
     let d = 1;
     let pointedIndex = -1;
