2ba4d1ea1731b64a97cd839520396037e01fa2fd
Kirill <kirillaromad@mail.ru>
2020-11-20 20:26:49 +0300

    up button

---
+++ HTML Coding/js/back-to-top.js
@@ -0,0 +1,20 @@
+let goTopBtn = document.querySelector('.rollover');
+
+window.addEventListener('scroll', trackScroll);
+goTopBtn.addEventListener('click', backToTop);
+
+function trackScroll() {
+    var scrolled = window.pageYOffset;
+    if (scrolled > 250) {
+        goTopBtn.classList.add('show');
+    } else {
+        goTopBtn.classList.remove('show');
+    }
+}
+
+function backToTop() {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -50);
+        setTimeout(backToTop, 0);
+    }
+}
