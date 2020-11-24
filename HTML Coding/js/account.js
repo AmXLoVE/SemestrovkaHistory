74928c80e85ecac478b4efc84f209b0d7cecf9c7
Kirill <kirillaromad@mail.ru>
2020-11-25 00:48:50 +0300

    try to add ajax account page

---
+++ HTML Coding/js/account.js
@@ -0,0 +1,11 @@
+function myArticles() {
+    $('body').load("account.html");
+}
+
+function myDiscussions() {
+    $('body').load("account-discussions.html");
+}
+
+function settings() {
+    $('body').load("account-settings.html");
+}
